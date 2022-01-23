8a5ba19d67cd392f1e299c5578adb118b085f040
unknown <patrickpobate@gmail.com>
2022-01-23 12:08:34 +0800

    making changes

--- src/components/Home/LilAbout/LilAboutComponents.js
+++ src/components/Home/LilAbout/LilAboutComponents.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AboutHSection = styled.div`
   /* padding: 30px 12.5vw 150px; */
-  padding: 1.5625vw 12.5vw 7.8125vw;
+  padding: 1.5625vw 12.5vw 10.8vw;
   z-index: 150;
   position: relative;
   background: #d7ddc1;
@@ -14,13 +14,13 @@ export const Header = styled.h2`
   font-size: 4.375vw;
   color: #7d8d6e;
   text-align: center;
+  margin-bottom: 0;
 `;
 
 export const Title = styled.h3`
   /* margin-top: 20px; */
-  margin-top: 1.0416666666666665vw;
+  margin-bottom: 1.04vw auto 1.2vw;
   /* margin-bottom: 20px; */
-  margin-bottom: 1.0416666666666665vw;
   text-align: center;
   /* font-size: 27px; */
   font-size: 1.40625vw;
